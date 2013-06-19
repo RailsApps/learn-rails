e98e7612d850db4e29faaa6d123d128b20223d48
Daniel Kehoe <daniel@danielkehoe.com>
2013-06-19 16:58:14 -0700

    contact form

---
+++ app/models/contact.rb
@@ -0,0 +1,14 @@
+class Contact < ActiveRecord::Base
+  has_no_table
+
+  column :name, :string
+  column :email, :string
+  column :content, :string
+
+  validates_presence_of :name
+  validates_presence_of :email
+  validates_presence_of :content
+  validates_format_of :email, :with => /\A[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}\z/i
+  validates_length_of :content, :maximum => 500
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 LearnRails::Application.routes.draw do
+  resources :contacts, only: [:new, :create]
   root :to => 'visitors#new'
 end
