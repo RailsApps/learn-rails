89bc27e350790793cd69986df3227ce6e9b94e01
Daniel Kehoe <daniel@danielkehoe.com>
2013-06-27 12:20:36 -0700

    fix event tracking

--- app/assets/javascripts/segmentio.js
+++ app/assets/javascripts/segmentio.js
@@ -1,9 +1,8 @@
 var analytics=analytics||[];(function(){var e=["identify","track","trackLink","trackForm","trackClick","trackSubmit","page","pageview","ab","alias","ready","group"],t=function(e){return function(){analytics.push([e].concat(Array.prototype.slice.call(arguments,0)))}};for(var n=0;n<e.length;n++)analytics[e[n]]=t(e[n])})(),analytics.load=function(e){var t=document.createElement("script");t.type="text/javascript",t.async=!0,t.src=("https:"===document.location.protocol?"https://":"http://")+"d2dq2ahtl5zl1z.cloudfront.net/analytics.js/v1/"+e+"/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(t,n)};
 analytics.load("YOUR_API_TOKEN");
-$(document).on('page:change', function() {
+$(document).on('page:load', function() {
+  console.log('page loaded');
   analytics.pageview();
-});
-var form_newsletter = $('#new_visitor');
-analytics.trackForm(form_newsletter, 'Signed Up');
-var form_contact = $('#new_contact');
-analytics.trackForm(form_contact, 'Contact Request');
+  analytics.trackForm($('#new_visitor'), 'Signed Up');
+  analytics.trackForm($('#new_contact'), 'Contact Request');
+})
