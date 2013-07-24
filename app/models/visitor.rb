5db2c8eb4373a1becac20c04510e4bf6f88f801f
Daniel Kehoe <daniel@danielkehoe.com>
2013-07-24 12:43:51 -0700

    new Gibbon API for MailChimp (monkeys throwing sh*t)

--- app/models/visitor.rb
+++ app/models/visitor.rb
@@ -5,17 +5,15 @@ class Visitor < ActiveRecord::Base
   validates_format_of :email, :with => /\A[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}\z/i
 
   def subscribe
-    mailchimp = Gibbon.new
-    result = mailchimp.list_subscribe({
+    mailchimp = Gibbon::API.new
+    result = mailchimp.lists.subscribe({
       :id => ENV['MAILCHIMP_LIST_ID'],
-      :email_address => self.email,
+      :email => {:email => self.email},
       :double_optin => false,
       :update_existing => true,
       :send_welcome => true
     })
     Rails.logger.info("Subscribed #{self.email} to MailChimp") if result
-  rescue Gibbon::MailChimpError => e
-    Rails.logger.info("MailChimp subscribe failed for #{self.email}: " + e.message)
   end
 
 end
