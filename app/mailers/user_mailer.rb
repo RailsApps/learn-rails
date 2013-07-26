88f26ecae3080f4bd64decca41d10633514c2600
Daniel Kehoe <daniel@danielkehoe.com>
2013-07-25 19:00:28 -0700

    update syntax for mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,8 +1,8 @@
 class UserMailer < ActionMailer::Base
-  default :from => "do-not-reply@example.com"
+  default from: "do-not-reply@example.com"
 
   def contact_email(contact)
     @contact = contact
-    mail(:to => ENV["OWNER_EMAIL"], :from => @contact.email, :subject => "Website Contact")
+    mail(to: ENV["OWNER_EMAIL"], from: @contact.email, :subject => "Website Contact")
   end
 end
