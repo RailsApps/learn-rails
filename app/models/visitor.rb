8c13706a10c6ed86647e7b8e1d8f1fb56bad4523
Daniel Kehoe <daniel@danielkehoe.com>
2014-05-05 14:30:17 -0700

    set mailchimp_api_key explicitly when instantiating Gibbon

--- app/models/visitor.rb
+++ app/models/visitor.rb
@@ -5,7 +5,7 @@ class Visitor < ActiveRecord::Base
   validates_format_of :email, :with => /\A[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}\z/i
 
   def subscribe
-    mailchimp = Gibbon::API.new
+    mailchimp = Gibbon::API.new(Rails.application.secrets.mailchimp_api_key)
     result = mailchimp.lists.subscribe({
       :id => Rails.application.secrets.mailchimp_list_id,
       :email => {:email => self.email},
