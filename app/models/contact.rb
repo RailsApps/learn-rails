275d73b197e3fa76acbc9c44b29db65a3b3560e5
Daniel Kehoe <daniel@danielkehoe.com>
2015-02-16 17:48:33 +0200

    Use the Ruby 1.9 hash syntax, closes #46

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -8,7 +8,7 @@ class Contact
   validates_presence_of :name
   validates_presence_of :email
   validates_presence_of :content
-  validates_format_of :email, :with => /\A[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}\z/i
+  validates_format_of :email, with: /\A[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}\z/i
   validates_length_of :content, :maximum => 500
 
   def update_spreadsheet
--- app/models/visitor.rb
+++ app/models/visitor.rb
@@ -2,7 +2,7 @@ class Visitor
   include ActiveModel::Model
   attr_accessor :email, :string
   validates_presence_of :email
-  validates_format_of :email, :with => /\A[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}\z/i
+  validates_format_of :email, with: /\A[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}\z/i
 
   def subscribe
     mailchimp = Gibbon::API.new(Rails.application.secrets.mailchimp_api_key)
