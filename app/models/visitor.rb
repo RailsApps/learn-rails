f4ff55b26a0bc84bdf1ef2a2f0cb8cb181dc7d4b
Daniel Kehoe <daniel@danielkehoe.com>
2015-09-04 15:57:09 +0800

    update Visitor model for Gibbon 2.0

--- app/models/visitor.rb
+++ app/models/visitor.rb
@@ -5,13 +5,12 @@ class Visitor
   validates_format_of :email, with: /\A[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}\z/i
 
   def subscribe
-    mailchimp = Gibbon::API.new(Rails.application.secrets.mailchimp_api_key)
-    result = mailchimp.lists.subscribe({
-      :id => Rails.application.secrets.mailchimp_list_id,
-      :email => {:email => self.email},
-      :double_optin => false,
-      :update_existing => true,
-      :send_welcome => true
+    mailchimp = Gibbon::Request.new(api_key: Rails.application.secrets.mailchimp_api_key)
+    list_id = Rails.application.secrets.mailchimp_list_id
+    result = mailchimp.lists(list_id).members.create(
+      body: {
+        email_address: self.email,
+        status: 'subscribed'
     })
     Rails.logger.info("Subscribed #{self.email} to MailChimp") if result
   end
