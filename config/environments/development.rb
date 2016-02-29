6b8e4f185ca6bbe25c134a88b75110e4de53b117
Daniel Kehoe <daniel@danielkehoe.com>
2016-02-29 17:15:48 +0800

    switch to using the SendGrid service to send email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,10 +28,12 @@ Rails.application.configure do
   config.assets.debug = true
 
   config.action_mailer.smtp_settings = {
-    address: "smtp.mandrillapp.com",
+    address: "smtp.sendgrid.net",
     port: 587,
+    domain: Rails.application.secrets.domain_name,
+    authentication: "plain",
     user_name: Rails.application.secrets.email_provider_username,
-    password: Rails.application.secrets.email_provider_api_key
+    password: Rails.application.secrets.email_provider_password
   }
   # ActionMailer Config
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
